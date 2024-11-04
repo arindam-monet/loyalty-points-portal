a57790fe2bd165923e6ab9bd52ab42bc343d9d94
arindammonet <arindam.d@monet.work>
2024-11-04 22:13:27 +0530

    fix: update env

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,11 +1,13 @@
 import { MongoClient } from 'mongodb';
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local');
-}
+
 
 const uri = process.env.MONGODB_URI;
 const options = {};
+
+if (!uri) {
+  throw new Error('Please add your Mongo URI to .env.local');
+}
 
 let client;
 let clientPromise: Promise<MongoClient>;
