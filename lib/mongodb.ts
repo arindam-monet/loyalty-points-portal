4d1f7fe9623167e64d0d89cf4fd7c61a646ddb77
arindammonet <arindam.d@monet.work>
2024-11-04 22:13:57 +0530

    fix: update env

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 
 
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI || 'https://mongodb-typescript-api.onrender.com';
 const options = {};
 
 if (!uri) {
